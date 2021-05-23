6c48952570bcd4debf3b24e85e9fe980066398f5
TomohiroKawakami <kyuprgnbrz@gmail.com>
2021-05-23 11:41:46 +0900

    Initial Commit

---
+++ YometaTests/YometaTests.swift
@@ -0,0 +1,33 @@
+//
+//  YometaTests.swift
+//  YometaTests
+//
+//  Created by 川上知宏 on 2021/05/23.
+//
+
+import XCTest
+@testable import Yometa
+
+class YometaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
